0cc20a9f518a2876589b9352e783ee51fcebf3bb
nazmulhossen1 <nazmulhossen.main@gmail.com>
2024-01-01 11:08:56 +0600

    Let, const and var deatails

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,3 +1,18 @@
 const accoutId = 13434;
 let accoutEamil = 'nazmulhossen539@gmail.com';
-var accoutPassword = 'password@1'
+var accoutPassword = 'password@1';
+accountCity = 'Bogura';
+let mangoState; // if you make a state and not a value on this state then the state deafult valu 'undefined'
+
+/* 
+Prefer not  to use var 
+beacuse of issue in block scope and functional scope
+*/
+
+// accountId =  234234 /* not allowed */
+
+
+accoutEamil = 'nazmulhossen@gmail.com';
+accoutPassword = 'password2';
+accountCity = 'Bogura | Sariakandi';
+console.table([accoutId,accoutEamil, accoutPassword, accountCity, mangoState]);
