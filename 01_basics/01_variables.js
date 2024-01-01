74ccec6d990ef01daec45e57f37c6a6b82adf72c
nazmulhossen1 <nazmulhossen.main@gmail.com>
2024-01-01 11:47:56 +0600

    Datatypes in js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accoutId = 13434;
-let accoutEamil = 'nazmulhossen539@gmail.com';
-var accoutPassword = 'password@1';
-accountCity = 'Bogura';
+let accoutEamil = "nazmulhossen539@gmail.com";
+var accoutPassword = "password@1";
+accountCity = "Bogura";
 let mangoState; // if you make a state and not a value on this state then the state deafult valu 'undefined'
 
 /* 
@@ -11,8 +11,7 @@ beacuse of issue in block scope and functional scope
 
 // accountId =  234234 /* not allowed */
 
-
-accoutEamil = 'nazmulhossen@gmail.com';
-accoutPassword = 'password2';
-accountCity = 'Bogura | Sariakandi';
-console.table([accoutId,accoutEamil, accoutPassword, accountCity, mangoState]);
+accoutEamil = "nazmulhossen@gmail.com";
+accoutPassword = "password2";
+accountCity = "Bogura | Sariakandi";
+console.table([accoutId, accoutEamil, accoutPassword, accountCity, mangoState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+" use strict"; //  treat all js code  as newer versions
+
+alert(4 + 4); // we are using nodejs, not browser | alert use browser right now
+
+console.log(45 + 43); // this  is readable for users
+
+console.log(45
+     + 
+     43); // code readibility sould be high
+
+
+
+let name = "Nazmul"  //string
+let age = 18 //Number
+let staus = false //boolean
+
+
+/* dataTypes */
+
+// Number => 2 to power 53
+//  bigint
+// string => "" | ''
+// boolean => true | false
+// null => standalone value || null is 'object' data type.[Interview Q ❓]
+// undefined => value not asing
+// Symbol => when we need unique, use symbol
+
+
+// object
+
+console.log(undefined); //undefined
+console.log(null); //object
+
+
