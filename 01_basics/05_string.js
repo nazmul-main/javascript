1d6fcf111864f3f608e691e41c1cfe4c08911f02
nazmulhossen1 <nazmulhossen.main@gmail.com>
2024-01-08 15:54:05 +0600

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,5 @@
+const name = 'Nazmul';
+const age = 23;
+
+console.log('My name is '+name ,'i am'+ age + 'years old' ); //This is not recommended.
+console.log(`I am ${name}, age is ${age}`);// Morden time 
